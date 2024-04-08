3033631ca56a1b99906dfa0a1491009632ade505
Itay Flikier <itay@mixtiles.com>
2024-04-08 18:25:17 +0300

    create new jobs

--- create_new_jobs.js
+++ create_new_jobs.js
@@ -37,7 +37,8 @@ async function getLastDate() {
     let lastDate = new Date();
     const keys = await redisClient.keys("bull:work:*")
     for (const key of keys) {
-        if (key.match(/bull:work:(\d)+/)) {
+        if (key.match(/bull:work:(\d)+$/)) {
+            log.info(`Create New Jobs - Checking key ${key}`)
             const dateStr = JSON.parse(await redisClient.hGet(key, "data"))["date"]
             const date = new Date(dateStr);
             if (date < lastDate) {
@@ -49,12 +50,16 @@ async function getLastDate() {
 }
   
 async function runNewJobs() {
+    log.info(`Create New Jobs - Starting...`);
+
     await redisClient.connect();
     numActiveJobs = await redisClient.lLen(`bull:work:active`);
 
     log.info(`Create New Jobs - Number of active jobs: ${numActiveJobs}`);
     if (numActiveJobs >= MAX_ACTIVE_JOBS) {
-        log.info(`Create New Jobs - There are enough active jobs (${numActiveJobs}), more then the maximum (${MAX_ACTIVE_JOBS}). Skipping...`)
+        log.info(`Create New Jobs - There are enough active jobs (${numActiveJobs}) - more than or exactly the maximum (${MAX_ACTIVE_JOBS}). Skipping...`)
+        await redisClient.disconnect();
+        process.exit(0);
         return;
     } else {
         log.info(`Create New Jobs - Going to create ${MAX_ACTIVE_JOBS - numActiveJobs} new jobs...`)
@@ -75,6 +80,8 @@ async function runNewJobs() {
         );
         log.info(`Create New Jobs - Job ${job.id} created for date ${date}`);
     }
+    log.info(`Create New Jobs - Finished!`);
+    await redisClient.disconnect();
     process.exit(0);
   }
 
