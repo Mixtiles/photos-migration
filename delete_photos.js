c6ed3e24216d4520453b1e039c2637cbdd96e553
Itay Flikier <itay@mixtiles.com>
2024-06-17 15:15:38 +0300

    delete photos

---
+++ delete_photos.js
@@ -0,0 +1,101 @@
+const { createClient } = require("redis");
+const { log } = require("./log");
+const cloudinary = require("cloudinary");
+
+const {
+  REDIS_URL,
+  DELETE_BATCH_SIZE,
+  DELETE_NUM_PHOTOS,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} = require("./env_vars");
+
+const DELETED_PHOTOS_SET = "deletedPhotos";
+const DELETED_PHOTOS_ERRORS_SET = "deletedPhotosErrors";
+const PHOTOS_TO_DELETE_SET = "photosToDelete";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+async function deletePhoto(photo, job) {
+  try {
+    const res = await cloudinary.v2.uploader.destroy(photo);
+    if (res.result === "ok") {
+      return true;
+    } else {
+      log.info(
+        `Job ${job.id}: Error deleting photo - unexpected result - ${photo}: ${res.result}`,
+      );
+      return false;
+    }
+  } catch (error) {
+    log.info(
+      `Job ${job.id}: Error deleting photo - exception - ${photo}: ${error.stack}`,
+    );
+    return false;
+  }
+}
+
+async function updateResult(photo, result, redisClient) {
+  if (result == true) {
+    await redisClient.sAdd(DELETED_PHOTOS_SET, photo);
+  } else {
+    await redisClient.sAdd(DELETED_PHOTOS_ERRORS_SET, photo);
+  }
+}
+
+async function deletePhotos(job) {
+  const redisClient = createClient({ url: REDIS_URL });
+  try {
+    await redisClient.connect();
+
+    const numBatches = Math.ceil(DELETE_NUM_PHOTOS / DELETE_BATCH_SIZE);
+    let numPhotosDeleted = 0;
+    let numPhotosErrored = 0;
+
+    log.info(
+      `Job ${job.id}: Going to delete ${DELETE_NUM_PHOTOS} photos in ${numBatches} batches of ${DELETE_BATCH_SIZE}`,
+    );
+
+    for (let i = 0; i < numBatches; i++) {
+      const photos = await redisClient.sPop(
+        PHOTOS_TO_DELETE_SET,
+        DELETE_BATCH_SIZE,
+      );
+      if (photos.length === 0) {
+        log.info(`Job ${job.id}: No more photos to delete`);
+        break;
+      }
+
+      const imageToResult = Object.fromEntries(
+        await Promise.all(
+          photos.map(async (photo) => [photo, await deletePhoto(photo, job)]),
+        ),
+      );
+
+      await Promise.all(
+        photos.map(async (photo) =>
+          updateResult(photo, imageToResult[photo], redisClient),
+        ),
+      );
+      numPhotosDeleted += Object.values(imageToResult).filter(Boolean).length;
+      numPhotosErrored += Object.values(imageToResult).filter((v) => !v).length;
+    }
+    log.info(
+      `Job ${job.id}: Done - Deleted ${numPhotosDeleted} photos, ${numPhotosErrored} errored`,
+    );
+  } catch (error) {
+    log.info(`Job ${job.id}: Error deleting photos - exception - ${error.stack}`);
+    return false;
+  } finally {
+    await redisClient.disconnect();
+  }
+}
+
+module.exports = {
+  deletePhotos,
+};
---
+++ worker_delete.js
@@ -0,0 +1,39 @@
+const throng = require('throng');
+const Queue = require("bull");
+const delete_photos = require('./delete_photos');
+const { redisOptions, WEB_CONCURRENCY } = require('./env_vars');
+const { log } = require('./log');
+
+// Spin up multiple processes to handle jobs to take advantage of more CPU cores
+// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+
+// The maximum number of jobs each worker should process at once. This will need
+// to be tuned for your application. If each job is mostly waiting on network 
+// responses it can be much higher. If each job is CPU-intensive, it might need
+// to be much lower.
+const maxJobsPerWorker = 50;
+
+function start() {
+
+  // Connect to the named work queue
+  const deleteQueue = new Queue('delete', { 
+    redis: redisOptions,
+    settings: { 
+      // If a worker doesn't report every 15 minutes, it is considered dead
+alledInterval: 15*60*100,
+      maxStalledCount: 100,
+      // Sometimes it takes it more than 5 minutes - so we just skip that test
+      skipStalledCheck: false,
+    }
+  });
+
+  deleteQueue.process(maxJobsPerWorker, async (job) => {
+    log.info(`Delete photos: Running job ${job.id}`);
+    job.progress(0);
+    return await delete_photos.deletePhotos(job);
+  });
+}
+
+// Initialize the clustered worker process
+// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+throng({ workers: WEB_CONCURRENCY, start });
